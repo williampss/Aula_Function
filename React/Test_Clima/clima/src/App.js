a8a0687614bf1da9b3d68b42c0150a81efe84929
William Patricio <williampatriciodosantos@gmail.com>
2024-05-24 04:10:57 -0300

    temp

--- React/Test_Clima/clima/src/App.js
+++ React/Test_Clima/clima/src/App.js
@@ -2,14 +2,15 @@ import {useState, useEffect} from 'react';
 import './App.css';
 import {  FaTemperatureHigh, FaThermometer, FaWater, FaWind } from "react-icons/fa";
 import { LuLocateFixed, LuText } from "react-icons/lu";
+import City from "./components/image/undraw_Mobile_user_re_xta4-removebg-preview.png"
 
 // import Clima from './components/clima/index';
 
 function App() {
   let temperatura= 30;
-  const [stateTemp, setstatTemp] = useState(0);
-  const [stateDescription, setstateDescription] = useState("Descrição");
-  const [stateWind, setstateWind] = useState(0);
+  const [stateTemp, setstatTemp] = useState(0)
+  const [stateDescription, setstateDescription] = useState("Descrição")
+  const [stateWind, setstateWind] = useState(0)
   const [cidade,setCidade] = useState("Local")
   const callApi = () =>{
   console.log('vai chamar na api')
@@ -24,7 +25,7 @@ function App() {
     setstatTemp(dadoTemperatura.main.temp)
    })
     .catch(() =>{
-      alert('Cidade não encontrada')
+      alert('LOCAL NÃO ENCONTRADO, TENTE NOVAMENTE😴')
     });
 }
 const dadoEntrada = (evento) =>{
@@ -41,22 +42,22 @@ const dadoEntrada = (evento) =>{
   // }
   return (
     <div className="App">
-       <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fundraw.co%2Fillustration%2Fcity_life_gnpr&psig=AOvVaw0UcN7kdiFVFzPMpIXBmiz5&ust=1716574377446000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOCPm9WwpIYDFQAAAAAdAAAAABAE'></img> 
+      <img src={City}/> 
       <div className='container'>
       <div className='inserir'>
          <input type='text' onChange={dadoEntrada}></input>
       </div>
-     
-      <button onClick= {callApi}>buscar</button>
+      <button onClick= {callApi}>Pesquisar</button>
+      <div className='boxHigh'>
       <p><LuLocateFixed/> {cidade}</p>
       <p><FaTemperatureHigh/> {stateTemp}</p>
+      </div>
+      <div className='boxDown'>
       <p><LuText/>{stateDescription}</p>
       <p><FaWind/> {stateWind} km/h</p>
-      
-     
       </div>
-     
-      
+
+      </div>
      
       {/* <butoon onClick= {() => {console.log(contador = contador + 1)}}>Clique Aqui</butoon>
       <button onClick ={()=> {setContadorEstado(contadorEstado + 1)}}>Clique Aquuui</button>
